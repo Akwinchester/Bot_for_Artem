ab1826e15e963f55c13c86de14bfe115c0a80c2b
AkWinchester <arsenijkiselev03@gmail.com>
2022-08-13 13:36:07 +0300

    Коммит стартовой точки перед началом финальной доработки

--- bot.py
+++ bot.py
@@ -7,6 +7,9 @@ from settings import tn
 from my_functions import add_row_for_csv_file, past_link
 from shutil import make_archive
 from google_sheets import GoogleSheet
+import requests
+from test import upload_to_folder
+
 
 bot = telebot.TeleBot(tn)
 
@@ -14,14 +17,19 @@ bot = telebot.TeleBot(tn)
 @bot.message_handler(commands=['start'])
 def welcome(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item_1 = types.KeyboardButton('Текст')
-    item_2 = types.KeyboardButton('Фото')
-    item_3 = types.KeyboardButton('Аудио')
-    item_4 = types.KeyboardButton('Видео')
+    item_1 = types.KeyboardButton('Фотографии в год открытия')
+    item_2 = types.KeyboardButton('Рисунки "Банк будущего"')
+    item_3 = types.KeyboardButton('Видео "Взгляд снизу"')
+    item_4 = types.KeyboardButton('Фотография с видом из окна офиса')
+    item_5 = types.KeyboardButton('5-кнопка')
+    item_6 = types.KeyboardButton('Задать вопрос')
     markup.add(item_1, item_2)
     markup.add(item_3, item_4)
+    markup.add(item_5, item_6)
 
     bot.send_message(message.chat.id, 'Привет, здесь ты можешь загрузить файлы', reply_markup=markup)
+
+
 
 
 @bot.message_handler(content_types=['text', 'photo', 'document', 'audio', 'video'])
@@ -29,7 +37,7 @@ def body(message):
     user_name = message.from_user.first_name
     gs = GoogleSheet()
     if message.text == 'Текст':
-        bot.send_message(message.chat.id, 'Отправте текстовый документ')
+        bot.send_message(message.chat.id, 'Отправьте текстовый документ')
     elif message.text == 'Фото':
         bot.send_message(message.chat.id, 'Отправьте фото')
     elif message.text == 'Видео':
@@ -45,11 +53,13 @@ def body(message):
                 f.write(downloaded_file)
 
             file_name = file_info.file_path.split('/')[-1]
+            upload_to_folder('1-a01zV7cZSYyO0G0XCDeVwg455ZgcThJ', f'./files/{file_info.file_path}', file_name)
             add_row_for_csv_file(user_name, message.content_type, file_name, f'./{file_info.file_path}')
-            gs.add('Лист1!A1', values=[user_name, past_link(file_name, f'./{file_info.file_path}')])
+            # gs.add('Лист1!A1', values=[user_name, past_link(file_name, f'./{file_info.file_path}')])
             bot.send_message(message.chat.id, 'фото загружено')
         except Exception as e:
             print('ошибка загрузки фото')
+            print(e)
             bot.send_message(message.chat.id, 'ошибка загрузки фото')
 
     elif message.document:
@@ -82,6 +92,7 @@ def body(message):
 
     elif message.video:
         try:
+            # file_id = message.document.file_id
             file_info = bot.get_file(message.video.file_id)
             downloaded_file = bot.download_file(file_info.file_path)
             with open(f'./files/{file_info.file_path}', 'wb') as f:
@@ -93,6 +104,7 @@ def body(message):
         except:
             print('ошибка загрузки видео')
             bot.send_message(message.chat.id, 'ошибка загрузки видео')
+            bot.copy_message(1536252416, from_chat_id=message.chat.id, message_id=message.message_id)
 
     shutil.make_archive('files_archive', 'zip', './files')
 
---
+++ google_drive.py
@@ -0,0 +1,63 @@
+from __future__ import print_function
+import os.path
+import pickle
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from settings import id_table
+
+SAMPLE_RANGE_NAME = 'Test List!A2:E246'
+
+
+class GoogleSheet:
+    SPREADSHEET_ID = id_table
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+    service = None
+    service_drive = None
+
+    def __init__(self):
+        creds = None
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                print('flow')
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', self.SCOPES)
+                creds = flow.run_local_server(port=0)
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+        self.service = build('sheets', 'v4', credentials=creds)
+        self.service_drive = service = build('drive', 'v3', credentials=creds)
+
+    def updateRangeValues(self, range, values):
+        data = [{
+            'range': range,
+            'values': values
+        }]
+        body = {
+            'valueInputOption': 'USER_ENTERED',
+            'data': data
+        }
+        result = self.service.spreadsheets().values().batchUpdate(spreadsheetId=self.SPREADSHEET_ID,
+                                                                  body=body).execute()
+        print('{0} cells updated.'.format(result.get('totalUpdatedCells')))
+
+    def add(self, range, values):
+
+        body = {'values':[values]}
+        result = self.service.spreadsheets().values().append(
+            spreadsheetId=self.SPREADSHEET_ID, range=range,
+            valueInputOption='USER_ENTERED', body=body).execute()
+    def n(self):
+        f = self.service_drive
+
+
+gs = GoogleSheet()
+gs.n()
